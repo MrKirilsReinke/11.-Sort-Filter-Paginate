a2a175af1cf6008ec9b16105712b16648bb025d4
MrKirilsReinke <120989319+MrKirilsReinke@users.noreply.github.com>
2023-02-02 17:29:21 +0200

    Add files via upload

---
+++ src/scripts.ts
@@ -0,0 +1,183 @@
+import axios from 'axios';
+
+// axios.get('http://localhost:3004/countries').then((response) => {
+//   console.log(response);
+// });
+
+axios.get('http://localhost:3004/countries').then((response) => {
+  createRow(response.data);
+});
+
+const tableBody = document.querySelector('tbody');
+
+const createRow = (data: any) => {
+  data.forEach((element: any) => {
+    const tableRow = document.createElement('tr');
+
+    tableRow.innerHTML = `
+    <th scope="row"><img src="" alt="${element.code}"></th>
+    <td>${element.region}</td>
+    <td>${element.name}</td>
+    <td>${element.capital}</td>
+    <td> 
+        <span class="symbol">
+            ${element.currency.symbol}
+        </span> 
+    ${element.currency.name}
+    </td>
+    <td>${element.language.name}</td>
+    `;
+
+    if (element.currency.symbol === null) {
+      tableRow.querySelector('.symbol').classList.add('hidden');
+    }
+
+    tableBody.appendChild(tableRow);
+
+    const tableRows = Array.from(tableBody.querySelectorAll('tr'));
+    let start = 0;
+    let end = 20;
+    const slice = tableRows.slice(start, end);
+    tableRows.forEach((tr) => {
+      tr.style.display = 'none';
+    });
+    slice.forEach((tr) => {
+      tr.style.display = 'table-row';
+    });
+
+    const loadMoreButton = document.querySelector('.load-more');
+    loadMoreButton.addEventListener('click', () => {
+      start = end;
+      end += 20;
+      const newSlice = tableRows.slice(start, end);
+      newSlice.forEach((tr) => {
+        tr.style.display = 'table-row';
+      });
+    });
+  });
+};
+
+function regionInputFunc() {
+  // Declare variables
+  let td; let i; let
+    txtValue;
+  const regionInput = document.getElementById('myRegion');
+  const filter = regionInput.value.toUpperCase();
+  const table = document.getElementById('myTable');
+  const tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i += 1) {
+    td = tr[i].getElementsByTagName('td')[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = null;
+      } else {
+        tr[i].style.visibility = 'collapse';
+      }
+    }
+  }
+}
+
+document.querySelector('#myRegion').addEventListener('input', () => regionInputFunc());
+
+function countryInputFunc() {
+  // Declare variables
+  let td; let i; let
+    txtValue;
+  const countryInput = document.getElementById('myCountry');
+  const filter = countryInput.value.toUpperCase();
+  const table = document.getElementById('myTable');
+  const tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i += 1) {
+    td = tr[i].getElementsByTagName('td')[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = null;
+      } else {
+        tr[i].style.visibility = 'collapse';
+      }
+    }
+  }
+}
+
+document.querySelector('#myCountry').addEventListener('input', () => countryInputFunc());
+
+function capitalInputFunc() {
+  // Declare variables
+  let td; let i; let
+    txtValue;
+  const capitalInput = document.getElementById('myCapital');
+  const filter = capitalInput.value.toUpperCase();
+  const table = document.getElementById('myTable');
+  const tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i += 1) {
+    td = tr[i].getElementsByTagName('td')[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = null;
+      } else {
+        tr[i].style.visibility = 'collapse';
+      }
+    }
+  }
+}
+
+document.querySelector('#myCapital').addEventListener('input', () => capitalInputFunc());
+
+function currencyInputFunc() {
+  // Declare variables
+  let td; let i; let
+    txtValue;
+  const currencyInput = document.getElementById('myCurrency');
+  const filter = currencyInput.value.toUpperCase();
+  const table = document.getElementById('myTable');
+  const tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i += 1) {
+    td = tr[i].getElementsByTagName('td')[3];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = null;
+      } else {
+        tr[i].style.visibility = 'collapse';
+      }
+    }
+  }
+}
+
+document.querySelector('#myCurrency').addEventListener('input', () => currencyInputFunc());
+
+function languageInputFunc() {
+  // Declare variables
+  let td; let i; let
+    txtValue;
+  const languageInput = document.getElementById('myLanguage');
+  const filter = languageInput.value.toUpperCase();
+  const table = document.getElementById('myTable');
+  const tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i += 1) {
+    td = tr[i].getElementsByTagName('td')[4];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = null;
+      } else {
+        tr[i].style.visibility = 'collapse';
+      }
+    }
+  }
+}
+
+document.querySelector('#myLanguage').addEventListener('input', () => languageInputFunc());
